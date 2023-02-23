e99f39ebc5d70b308f91eda807ef79f92c419b76
Eco <osw2156@gmail.com>
2023-02-23 01:30:16 -0600

    Diccionarios y Modulo de Usuarios Proyecto

---
+++ ListasDiccionarios/diccionarios.py
@@ -0,0 +1,9 @@
+#Diccionarios Ejemplo
+
+dic_user = {}
+
+dic_user['user1'] = 'Oswald', 'a'
+dic_user['user2'] = 'Andy'
+dic_user['user3'] = 'Dani'
+
+print(dic_user['user1'][0])
--- Proyecto/ProyectoClinicaMascotas.py
+++ Proyecto/ProyectoClinicaMascotas.py
@@ -2,6 +2,7 @@
 
 #Librerias
 import os 
+import time
 
 # - logeo -
 os.system('cls')
@@ -17,53 +18,89 @@ password_2 = "1234"
 userName_3 = "oswald"
 password_3 = "1234"
 
+user_diccionario = {}
+
+
 
 #Validar Login
-a = 1 #Bandera para validar cuando el usuario y la contrasena son correctos.
-while a == 1:
+login_Hilo_1 = 1 #Bandera para validar cuando el usuario y la contrasena son correctos.
+while login_Hilo_1 == 1:
     user = input("Digite su Usuario: ")
     if user == userName_1 or user == userName_2 or user == userName_3:
-        b = 1
-        while b == 1:
+        login_Hilo_2 = 1
+        while login_Hilo_2 == 1:
             password = input("Digite su Password: ")
             if password == password_1 or user == password_2 or user == password_3:
-                b = 0
-                a = 0
+                login_Hilo_2 = 0
+                login_Hilo_1 = 0
                 print(f"\nBienvenid@ {user}\n")
             else:
-                b = 1
+                login_Hilo_2 = 1
                 os.system('cls')
                 print("\n- Login -")
                 print("\nAcceso Denegado Contraseña Incorrecta\n")
                 print(f"Digite su Usuario: {user}")
     else: 
-        a = 1
+        login_Hilo_1 = 1
         os.system('cls')
         print("\n- Login -")
         print("\nAcceso Denegado Usuario Incorrecto\n")
 
+# - Modulos -
+def modulo_usuarios():
+    
+    modulo_Usuarios_Hilo = 1
+
+    while modulo_Usuarios_Hilo == 1:
+        os.system("cls")
+        print("\n-- Módulo De Usuarios -- \n")
+        print(" 1. Registrar Usuarios")
+        print(" 2. Consultar Usuarios")
+        print(" 3. Exit")
+
+        modulo_Usuarios_Seleccion = int(input("\nDigite una opción: "))
+       
+        if modulo_Usuarios_Seleccion == 1:
+            os.system('cls')
+            print("\n-- Registrar Usuarios -- \n")
+            user_name = input("\nNombre: ")
+            user_pass = input("\nContraseña: ")
+            user_diccionario[user_name] = user_name,user_pass
+            print(f"\nRegistrado Exitosamente Usuario {user_name}")
+            time.sleep(1) 
+            
+        elif modulo_Usuarios_Seleccion == 2:
+            os.system('cls')
+            print("\n-- Consultar Usuarios -- \n")
+            user_name = input("\nNombre: ")
+            try:
+                print(f"\nNombre: {user_diccionario[user_name][0]} Contraseña: {user_diccionario[user_name][1]} ")
+                time.sleep(2) 
+            except:
+                print("El Usuario no existe")
+
+        elif modulo_Usuarios_Seleccion <= 3 and modulo_Usuarios_Seleccion != 0: 
+            modulo_Usuarios_Hilo = 0
+            os.system('cls')
+
 # - Menu Prinpicial - 
+
 os.system("cls")
-a = 1
-while a == 1: 
+menu_Hilo = 1
+while menu_Hilo == 1: 
     print("\n-- Menú Principal -- \n")
-    print("1. Módulo Registros")
-    print("2. Módulo Clínica")
-    print("3. Módulo de Venta de Productos")
-    print("4. Historial:")
-    print("5. Exit")
+    print(" 1. Módulo Registros")
+    print(" 2. Módulo Clínica")
+    print(" 3. Módulo de Venta de Productos")
+    print(" 4. Módulo de Usuarios")
+    print(" 5. Historial:")
+    print(" 6. Exit")
     menu_Seleccion = int(input("\nDigite una opción: "))
 
-    if menu_Seleccion <= 5 and menu_Seleccion != 0: 
-        a = 0
-    else:
-        os.system("cls")
-        a = 1
-
-# - Modulos -
-
-#Módulo Registros
-
+    if  menu_Seleccion == 4:    
+        modulo_usuarios()
+    elif menu_Seleccion <= 6 and menu_Seleccion != 0: 
+        menu_Hilo = 0
 
 
 
