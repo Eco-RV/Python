05d0442f2899babc50edd6e294fbfbbd929029a8
Eco <osw2156@gmail.com>
2023-02-23 00:04:25 -0600

    Update loops.py

--- HelloWorld/loops.py
+++ HelloWorld/loops.py
@@ -1,6 +1,9 @@
-
-
-
-for i in range(3):
-    for j in range(3):
-        print("*") 
+num = int(input("Digite el numero de escalones: "))
+a = 1
+for i in range(a,num+1):
+    b = 0
+    for j in range(b,i):
+        print("*",end = ' ')
+        b+=1
+    print("")
+    
