76cf4b79c8ce048cbe591198a12492a260307371
Eco <osw2156@gmail.com>
2023-02-25 00:39:18 -0600

    Registros

--- Proyecto/ProyectoClinicaMascotas.py
+++ Proyecto/ProyectoClinicaMascotas.py
@@ -2,11 +2,10 @@
 
 #Librerias
 import os 
-import time
 
 # - logeo -
 os.system('cls')
-print("\n- Login - \n")
+print("\n - Login - \n")
 
 #Usuarios
 userName_1 = "maria" 
@@ -18,90 +17,119 @@ password_2 = "1234"
 userName_3 = "oswald"
 password_3 = "1234"
 
-user_diccionario = {}
-
-
 
 #Validar Login
-login_Hilo_1 = 1 #Bandera para validar cuando el usuario y la contrasena son correctos.
-while login_Hilo_1 == 1:
+while True:
     user = input("Digite su Usuario: ")
     if user == userName_1 or user == userName_2 or user == userName_3:
-        login_Hilo_2 = 1
-        while login_Hilo_2 == 1:
+        while True:
             password = input("Digite su Password: ")
             if password == password_1 or user == password_2 or user == password_3:
-                login_Hilo_2 = 0
-                login_Hilo_1 = 0
                 print(f"\nBienvenid@ {user}\n")
+                break
             else:
-                login_Hilo_2 = 1
                 os.system('cls')
                 print("\n- Login -")
                 print("\nAcceso Denegado Contraseña Incorrecta\n")
                 print(f"Digite su Usuario: {user}")
-    else: 
-        login_Hilo_1 = 1
+    else:
         os.system('cls')
         print("\n- Login -")
-        print("\nAcceso Denegado Usuario Incorrecto\n")
-
-# - Modulos -
-def modulo_usuarios():
-    
-    modulo_Usuarios_Hilo = 1
-
-    while modulo_Usuarios_Hilo == 1:
-        os.system("cls")
-        print("\n-- Módulo De Usuarios -- \n")
-        print(" 1. Registrar Usuarios")
-        print(" 2. Consultar Usuarios")
-        print(" 3. Exit")
-
-        modulo_Usuarios_Seleccion = int(input("\nDigite una opción: "))
-       
-        if modulo_Usuarios_Seleccion == 1:
-            os.system('cls')
-            print("\n-- Registrar Usuarios -- \n")
-            user_name = input("\nNombre: ")
-            user_pass = input("\nContraseña: ")
-            user_diccionario[user_name] = user_name,user_pass
-            print(f"\nRegistrado Exitosamente Usuario {user_name}")
-            time.sleep(1) 
-            
-        elif modulo_Usuarios_Seleccion == 2:
-            os.system('cls')
-            print("\n-- Consultar Usuarios -- \n")
-            user_name = input("\nNombre: ")
-            try:
-                print(f"\nNombre: {user_diccionario[user_name][0]} Contraseña: {user_diccionario[user_name][1]} ")
-                time.sleep(2) 
-            except:
-                print("El Usuario no existe")
-
-        elif modulo_Usuarios_Seleccion <= 3 and modulo_Usuarios_Seleccion != 0: 
-            modulo_Usuarios_Hilo = 0
-            os.system('cls')
+        print("\nAcceso Denegado Usuario Incorrecto\n") 
+        break
 
 # - Menu Prinpicial - 
-
 os.system("cls")
-menu_Hilo = 1
-while menu_Hilo == 1: 
+while True: 
+    
     print("\n-- Menú Principal -- \n")
     print(" 1. Módulo Registros")
     print(" 2. Módulo Clínica")
     print(" 3. Módulo de Venta de Productos")
-    print(" 4. Módulo de Usuarios")
-    print(" 5. Historial:")
-    print(" 6. Exit")
-    menu_Seleccion = int(input("\nDigite una opción: "))
+    print(" 4. Historial:")
+    print(" 5. Exit")
 
-    if  menu_Seleccion == 4:    
-        modulo_usuarios()
-    elif menu_Seleccion <= 6 and menu_Seleccion != 0: 
-        menu_Hilo = 0
+    menu_Seleccion = (input("\nDigite una opción: "))
 
+    if menu_Seleccion == "1":
+            print("\n_Módulo Registros_\n")
+            while True:
+
+                # - Modulos -
+                os.system("cls")
+                print(" 1. Registros de Médicos.")
+                print(" 2. Datos de dueño.")
+                print(" 3. Datos de las mascotas.")
+                print(" 4. Salir")
+
+                registro_Seleccion = (input("\nDigite una opción: "))
+                
+                if   registro_Seleccion == "1":
+
+                    print("\n_Registro de Medicos_\n" )
+                    
+                    if   med_nomDoc_1 == "":
+
+                        med_nomDoc_1       = input("Digite el nombre del Doctor Veterinario: ")
+                        med_codigo_1       = input("Digite el código: ")
+                        med_espe_1         = input("Digite la especialidad: ")
+                        med_tel_1          = int(input("Telefono: "))
+                        med_correo_1       = input("Correo electronico: ")
+                        med_dispoHorario_1 = input("Disponibilidad: ")
+
+                    elif med_nomDoc_2 == "":
+
+                        med_nomDoc_2       = input("Digite el nombre del Doctor Veterinario: ")
+                        med_codigo_2       = input("Digite el código: ")
+                        med_espe_2         = input("Digite la especialidad: ")
+                        med_tel_2          = int(input("Telefono: "))
+                        med_correo_2       = input("Correo electronico: ")
+                        med_dispoHorario_2 = input("Disponibilidad: ")
+                    
+                    elif med_nomDoc_3 == "":
+
+                        med_nomDoc_3       = input("Digite el nombre del Doctor Veterinario: ")
+                        med_codigo_3       = input("Digite el código: ")
+                        med_espe_3         = input("Digite la especialidad: ")
+                        med_tel_3          = int(input("Telefono: "))
+                        med_correo_3       = input("Correo electronico: ")
+                        med_dispoHorario_3 = input("Disponibilidad: ")
+
+                    else:
+                        print("Máximo de Médicos Registrados.")
+
+                elif registro_Seleccion == "2":
+                    nombre = input("Ingrese el nombre completo del dueño: ")
+                    cedula = input("Ingrese la cédula del dueño: ")
+                    telefono = input("Ingrese el número de teléfono del dueño: ")
+                    correo = input("Ingrese el correo del dueño: ")
+                    direccion = input("Ingrese la dirección del dueño: ")
+                    
+                elif registro_Seleccion == "3":
+                    nombre_mascota = input("Ingrese el nombre de la mascota: ")
+                    raza = input("Ingrese la raza de la mascota: ")
+                    fecha_nacimiento = input("Ingrese la fecha de nacimiento de la mascota (formato dd/mm/aaaa): ")
+                    sexo = input("Ingrese el sexo de la mascota (Macho/Hembra): ")
+                    peso = float(input("Ingrese el peso de la mascota (en kilogramos): "))
+                    caracteristicas = input("Ingrese las características de la mascota: ")
+                    alimento = input("Ingrese el alimento que consume la mascota: ")
+                    observaciones = input("Ingrese observaciones generales de la mascota: ")
+                    cedulaDueño= input("Ingrese el número de cedula del dueño: ")
+                    print()
+                    print("Se han cargado los datos de las mascotas exitosamente.")
+                
+                elif registro_Seleccion == "4":
+                    break
+                
+                else:
+                    print("No has pulsado una opcion correcta." )
+    
+    elif menu_Seleccion == "5":
+        break    
+    else:
+        print("No has pulsado una opcion correcta.")  
+
+    
 
 
 
