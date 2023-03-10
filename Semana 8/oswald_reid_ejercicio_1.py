1a61721367e6f2c05578db7bd2710b65463cb14a
Eco <osw2156@gmail.com>
2023-03-09 22:17:52 -0600

    Ejercicio 1 1.1

--- Semana 8/oswald_reid_ejercicio_1.py
+++ Semana 8/oswald_reid_ejercicio_1.py
@@ -13,9 +13,10 @@ entrada_precio_Infantiles = 5000
 entrada_precio_extremos   = 7000
 entrada_precio_maxima     = 9000
 
-descuento_4   = 0.05
-descuento_8   = 0.10
-descuento_Max = 0.05
+descuento_5          = 0.05
+descuento_10         = 0.10
+descuento_15         = 0.15
+descuento_porcentaje = "%00"
 
 iva = 0.13
 
@@ -24,6 +25,19 @@ factura_cantidad_personas_total = 0
 factura_descuento               = 0
 factura_iva                     = 0
 factura_total                   = 0
+factura_subtotal                = 0
+
+regular_cantidad_personas   = 0
+regular_monto_pagar         = 0
+ 
+maxima_monto_pagar          = 0
+maxima_cantidad_personas    = 0
+ 
+extremos_monto_pagar        = 0
+extremos_cantidad_personas  = 0
+ 
+infantil_monto_pagar        = 0
+infantil_cantidad_personas  = 0
 
 #Funciones Begin
 
@@ -38,13 +52,17 @@ def factura_acumular_montos(monto_entrada,cantidad_personas):
 def factura_Calcula_descuento():
 
     global factura_descuento
+    global descuento_porcentaje
 
-    if factura_cantidad_personas_total == 4:
-        factura_descuento = factura_entrada_monto_total * descuento_4
+    if factura_cantidad_personas_total >= 4 and factura_cantidad_personas_total < 8:
+        factura_descuento    = factura_entrada_monto_total * descuento_5
+        descuento_porcentaje = "05%"
     elif factura_cantidad_personas_total == 8:
-        factura_descuento = factura_entrada_monto_total * descuento_8
+        factura_descuento    = factura_entrada_monto_total * descuento_10
+        descuento_porcentaje = "10%"
     elif factura_cantidad_personas_total > 8:
-        factura_descuento = factura_entrada_monto_total * descuento_Max
+        factura_descuento    = factura_entrada_monto_total * descuento_15
+        descuento_porcentaje = "15%"
     else:
         factura_descuento = 0
 
@@ -56,14 +74,29 @@ def factura_Calcula_IVA():
     
 def factura_Calcula_Monto_Total():
     global factura_total
+    global factura_subtotal
 
-    factura_total = (factura_entrada_monto_total - factura_descuento) + iva
+    factura_total = (factura_entrada_monto_total - factura_descuento) + factura_iva
+    factura_subtotal = regular_monto_pagar + infantil_monto_pagar + extremos_monto_pagar + maxima_monto_pagar
+
 
 #Funciones End
 
 #Login Begin
 
-#De ultimo para el nombre del cliente
+login_hilo = True
+while login_hilo:
+    os.system("cls")
+    print(" ╔══════════════════════════════════════╗")
+    print(" ║               Ingreso                ║")
+    print(" ╚══════════════════════════════════════╝")
+
+    login_client_nombre = input("  Digite su Nombre  : ")
+    login_client_nombre_confirmacion = input("  Confirme su Nombre: ")
+
+    if login_client_nombre_confirmacion.upper() == login_client_nombre.upper():
+        login_hilo = False
+        
 
 #Login End
 
@@ -74,18 +107,24 @@ while menu_hilo:
 
     #Bienvenida
     os.system("cls")
-    print("-- Parque de Diversiones --")
-    print("\n-- Bienvenid@ --\n")
+    print(" ╔══════════════════════════════════════╗")
+    print(" ║ Parque de Atracciones Studios Ghibli ║")
+    print(" ╚══════════════════════════════════════╝")
+
+    print(f"            Bienvenid@ {login_client_nombre}          ")
+    print("  ══════════════════════════════════════")
 
     #Impresion del Menu
-    print(" 1. Entrada regular")
-    print(" 2. Infantiles")
-    print(" 3. Familiares extremos")
-    print(" 4. Adrenalina máxima")
-    print(" 5. Imprime factura")
-    print(" 6. Exit")
+    print(" ╔══════════════════════════════════════╗")
+    print(" ║ 1. Entrada regular                   ║")
+    print(" ║ 2. Infantiles                        ║")
+    print(" ║ 3. Familiares extremos               ║")
+    print(" ║ 4. Adrenalina máxima                 ║")
+    print(" ║ 5. Imprime factura                   ║")
+    print(" ║ 6. Exit                              ║")
+    print(" ╚══════════════════════════════════════╝")
 
-    menu_opcion = int(input("\n Digite una Opción: "))
+    menu_opcion = int(input("\n   Digite una Opción: "))
 
     if menu_opcion == 1:
 
@@ -95,8 +134,8 @@ while menu_hilo:
         print(" ║ Entrada Regular por Persona: ¢2000  ║")
         print(" ╚═════════════════════════════════════╝")
 
-        regular_cantidad_personas = int(input("\nDigite la Cantidad de Personas: "))
-        regular_monto_pagar = entrada_precio_regular * regular_cantidad_personas 
+        regular_cantidad_personas   += int(input("\n  Digite la Cantidad de Personas: "))
+        regular_monto_pagar         += entrada_precio_regular * regular_cantidad_personas 
 
         factura_acumular_montos(regular_monto_pagar,regular_cantidad_personas)
 
@@ -108,8 +147,8 @@ while menu_hilo:
         print(" ║ Entradas Infantiles por Persona: ¢5000 ║")
         print(" ╚════════════════════════════════════════╝")
 
-        infantil_cantidad_personas = int(input("\nDigite la Cantidad de Niñ@s: "))
-        infantil_monto_pagar = entrada_precio_Infantiles * infantil_cantidad_personas 
+        infantil_cantidad_personas  += int(input("\n  Digite la Cantidad de Niñ@s: "))
+        infantil_monto_pagar        += entrada_precio_Infantiles * infantil_cantidad_personas 
 
         factura_acumular_montos(infantil_monto_pagar,infantil_cantidad_personas)
 
@@ -121,8 +160,8 @@ while menu_hilo:
         print(" ║ Familiares extremos por Persona: ¢7000 ║")
         print(" ╚════════════════════════════════════════╝")
 
-        extremos_cantidad_personas = int(input("\nDigite la Cantidad de Personas: "))
-        extremos_monto_pagar = entrada_precio_extremos * extremos_cantidad_personas 
+        extremos_cantidad_personas += int(input("\n  Digite la Cantidad de Personas: "))
+        extremos_monto_pagar       += entrada_precio_extremos * extremos_cantidad_personas 
 
         factura_acumular_montos(extremos_monto_pagar,extremos_cantidad_personas)
 
@@ -134,24 +173,60 @@ while menu_hilo:
         print(" ║ Adrenalina máxima por Persona: ¢9000   ║")
         print(" ╚════════════════════════════════════════╝")
 
-        maxima_cantidad_personas = int(input("\nDigite la Cantidad de Personas: "))
-        maxima_monto_pagar = entrada_precio_maxima * maxima_cantidad_personas
+        maxima_cantidad_personas += int(input("\n  Digite la Cantidad de Personas: "))
+        maxima_monto_pagar       += entrada_precio_maxima * maxima_cantidad_personas
         factura_acumular_montos(maxima_monto_pagar,maxima_cantidad_personas)
 
     elif menu_opcion == 5:
 
+        factura_hilo = True
+        while factura_hilo:
 
-        factura_Calcula_descuento()
-        factura_Calcula_IVA()
-        factura_Calcula_Monto_Total()
+            #Calculo de Montos
+            factura_Calcula_descuento()
+            factura_Calcula_IVA()
+            factura_Calcula_Monto_Total()
 
-        print(factura_entrada_monto_total)
-        print(factura_cantidad_personas_total)
-        print(factura_descuento)
-        print(factura_iva)
-        print(factura_total)
+            os.system("cls")
 
-        time.sleep(2)
+            print(" ╔═══════════════════════════════════════════════════════════╗")
+            print(" ║          Parque de Atracciones Studios Ghibli             ║")
+            print(" ╚═══════════════════════════════════════════════════════════╝")
+            print(f"   Nombre del Cliente: {login_client_nombre}")
+            print(" ╔═══════════════════════════════════════════════════════════╗")
+            print(" ║ PAQUETE                CANTIDAD               TOTAL       ║")
+            print(" ╚═══════════════════════════════════════════════════════════╝")
+
+            if regular_cantidad_personas > 0:
+                print(f"   Entrada regular            {regular_cantidad_personas}                  {regular_monto_pagar}        ")
+                print(f"  ═══════════════════════════════════════════════════════════")
+
+            if infantil_cantidad_personas > 0:
+                print(f"   Infantiles                 {infantil_cantidad_personas}                  {infantil_monto_pagar}       ")
+                print(f"  ═══════════════════════════════════════════════════════════")
+
+            if extremos_cantidad_personas > 0:
+                print(f"   Familiares extremos        {extremos_cantidad_personas}                  {extremos_monto_pagar}       ")
+                print(f"  ═══════════════════════════════════════════════════════════")
+
+            if maxima_cantidad_personas > 0:
+                print(f"   Adrenalina máxima          {maxima_cantidad_personas}                  {maxima_monto_pagar}           ")
+                print(f"  ═══════════════════════════════════════════════════════════")
+            
+            print(f"   SubTotal      : {factura_subtotal}")
+            print(f"   Descuento({descuento_porcentaje}): {factura_descuento}")
+            print(f"   IVA           : {factura_iva}")
+            print(f"   Total         : {factura_total}")
+
+            print(" ╔═══════════════════════════════════════════════════════════╗")
+            print(" ║                           FIN                             ║")
+            print(" ╚═══════════════════════════════════════════════════════════╝")
+    
+            factura_salir = input("\n   ¿Desea Salir? (Y): ")
+            
+            if factura_salir.upper() == 'Y':
+                factura_hilo = False
+
     elif menu_opcion == 6:
         menu_hilo = False
 
